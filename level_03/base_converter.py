97daff1258d9ee6a84ac74dd7859e1860725fb5a
Maria Paula Almeida De Freitas <mariaalm@c1r2s3.42porto.com>
2026-05-29 19:06:07 +0100

    change the txt

--- level_03/base_converter.py
+++ level_03/base_converter.py
@@ -2,6 +2,10 @@ def number_base_converter(num: str, src_base: int, dest_base: int) -> str:
     digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     try:
+    	if not 2 <= from_base <= 36:
+            return "ERROR"
+        if not 2 <= to_base <= 36:
+            return "ERROR"
         n = int(num, src_base)
 
         if n == 0:
