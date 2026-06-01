3584139491c2a99cddd8a1fdd742af4781efef3c
Maria Paula <mariapaula.almeidafr@gmail.com>
2026-06-01 17:50:35 +0100

    Update sort_list.py

--- level_01/sort_list.py
+++ level_01/sort_list.py
@@ -3,5 +3,5 @@ def sort(str_list: list[str]) -> list[str]:
     def key_func(s):
         len_s = len(s)
         vogal_count = sum(1 for c in s for c in vogal)
-            return (len_s, s.lower(), vogal_count)
+        return (len_s, s.lower(), vogal_count)
     return sorted(str_list, key=key_func)
