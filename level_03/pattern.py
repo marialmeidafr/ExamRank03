919ff7f70748822d941ae542654434915df689d3
Maria Paula <mariapaula.almeidafr@gmail.com>
2026-06-09 14:39:09 +0100

    Fix condition in pattern_tracker function

--- level_03/pattern.py
+++ level_03/pattern.py
@@ -1,6 +1,6 @@
 def pattern_tracker(s: str) -> int:
     count = 0
     for i in range(len(s) - 1):
-        if s[i].isdigit() and s[i+1].isdigit() and s[i+1] > s[i] and int(s[i+1]) == int(s[i] + 1):
+        if s[i].isdigit() and s[i+1].isdigit() and s[i+1] > s[i] and int(s[i+1]) == int(s[i]) + 1:
                 count += 1
     return count
